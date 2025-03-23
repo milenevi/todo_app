ad0b7315e90c1e279fdddcc39c8e4e0fff44543d
Milene <milene_vi@hotmail.com>
2025-03-23 20:53:42 -0300

    feat: add model Todo

---
+++ lib/models/todo.dart
@@ -0,0 +1,45 @@
+class Todo {
+  final int id;
+  final String todo;
+  final bool completed;
+  final int userId;
+
+  Todo({
+    required this.id,
+    required this.todo,
+    required this.completed,
+    required this.userId,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'],
+      todo: json['todo'],
+      completed: json['completed'],
+      userId: json['userId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'todo': todo,
+      'completed': completed,
+      'userId': userId,
+    };
+  }
+
+  Todo copyWith({
+    int? id,
+    String? todo,
+    bool? completed,
+    int? userId,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      todo: todo ?? this.todo,
+      completed: completed ?? this.completed,
+      userId: userId ?? this.userId,
+    );
+  }
+}
