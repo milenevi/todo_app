4be6cab5e3061e3725c9cb2fcbd9e68ef264be92
Milene <milene_vi@hotmail.com>
2025-03-23 20:34:55 -0300

    refactor: add provider and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/screens/todo_list_creen.dart';
+import 'providers/todo_provider.dart';
+import 'theme/app_themes.dart';
+import 'theme/theme_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TodoProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'Todo App',
+            theme: AppThemes.getLightTheme(),
+            darkTheme: AppThemes.getDarkTheme(),
+            themeMode:
+            themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            home: const TodoListScreen(),
+          );
+        },
       ),
-      home: const TodoListScreen(),
     );
   }
 }
